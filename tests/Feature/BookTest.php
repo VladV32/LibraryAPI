8da26f3ba05bd1749d16d2199a2a1c5ea1d0837c
Vlad Vodolazskij <vlad.vodolazskij.32@gmail.com>
2024-05-11 19:55:56 +0300

    create BookTest

---
+++ tests/Feature/BookTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BookTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutMiddleware();
+    }
+
+    public function test_get_list_of_books()
+    {
+        Book::factory()->count(3)->create();
+
+        $response = $this->get('/api/books');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(3, 'data');
+    }
+
+    public function test_create_new_book()
+    {
+        $bookData = [
+            'title' => 'Sample Book',
+            'publisher' => 'Publisher Name',
+            'author' => 'Author Name',
+            'genre' => 'Fiction',
+            'publication_date' => '2022-01-01',
+            'word_count' => 50000,
+            'price' => 25.99,
+        ];
+
+        $response = $this->post('/api/books', $bookData);
+
+        $response->assertStatus(201);
+        $this->assertDatabaseHas('books', $bookData);
+    }
+
+    public function test_show_book()
+    {
+        $book = Book::factory()->create();
+
+        $response = $this->get('/api/books/'.$book->id);
+
+        $response->assertStatus(200);
+        $response->assertJson(['data' => $book->toArray()]);
+    }
+
+    public function test_update_book()
+    {
+        $book = Book::factory()->create();
+
+        $updatedBookData = [
+            'title' => 'Updated Book Title',
+            'publisher' => 'Updated Publisher Name',
+        ];
+
+        $response = $this->put('/api/books/'.$book->id, $updatedBookData);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('books', $updatedBookData);
+    }
+
+    public function test_delete_book()
+    {
+        $book = Book::factory()->create();
+
+        $response = $this->delete('/api/books/'.$book->id);
+
+        $response->assertStatus(204);
+        $this->assertDatabaseMissing('books', ['id' => $book->id]);
+    }
+
+}
